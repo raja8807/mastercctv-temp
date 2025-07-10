cfe3e98646adf24843089698bd015e7e15b55f91
Raja <raja.rathinam@pcisage.com>
2025-07-10 19:23:10 +0530

    testimonials

--- src/components/screens/home/sections/testimonials/testimonials.jsx
+++ src/components/screens/home/sections/testimonials/testimonials.jsx
@@ -9,19 +9,19 @@ import { StarFill } from "react-bootstrap-icons";
 const TestimonialsSection = () => {
   const testimonials = [
     {
-      text: "The technician was very courteous and informative about the new cameras that I didn’t even know was out there. My cameras are old and keeps going into night mode. I will be getting new cameras. I thank the technician who showed me a new view of what you can see with new cameras and I like it!",
-      name: " Sami Wade",
-      location: "Promina Inc",
+      text: "I'm really happy with the CCTV setup provided by Master. The team was punctual, polite, and handled everything professionally. The video clarity is excellent, even at night, and the mobile app makes it easy to monitor things remotely. I truly appreciate their quality service and support!"
+      ,name: "Aarthie",
+      location: "Seyon Group Of Companies, Chennai",
     },
     {
-      text: "The technician was very courteous and informative about the new cameras that I didn’t even know was out there. My cameras are old and keeps going into night mode. I will be getting new cameras. I thank the technician who showed me a new view of what you can see with new cameras and I like it!",
-      name: " Sami Wade",
-      location: "Promina Inc",
+      text: "I installed CCTV from Master for my consultancy office, mainly to ensure safety during the night shift. Their service was smooth and professional from start to finish. The night vision quality is impressive, and it gives me complete peace of mind knowing everything is being monitored, even after hours. I highly recommend them for office security solutions.",
+      name: "Nantha",
+      location: "Forgin education consultant. Armenia",
     },
     {
-      text: "The technician was very courteous and informative about the new cameras that I didn’t even know was out there. My cameras are old and keeps going into night mode. I will be getting new cameras. I thank the technician who showed me a new view of what you can see with new cameras and I like it!",
-      name: " Sami Wade",
-      location: "Promina Inc",
+      text: "I recently installed a CCTV system from Master to protect myself and my company from potential threats like criminals and theft. The system includes an advanced alarm feature that adds an extra layer of security. I'm extremely satisfied with the product quality and their prompt, professional service. I feel much more secure now, thanks to Master.",
+      name: "Harsavarthan",
+      location: "VVR Traders, South end circle.",
     },
   ];
 
